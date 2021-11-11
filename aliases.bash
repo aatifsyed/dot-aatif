1457d616f59f007e8a8feb700bc024167153b986
Aatif Syed <aatif@aatifsyed.uk>
2021-11-11 20:36:34 +0000

    fzf tweaks

--- aliases.bash
+++ aliases.bash
@@ -2,9 +2,11 @@
 
 # Convenience
 alias bashrc="source ~/.bashrc"
+alias bf="bat \$(fzf)"
 alias c="code ."
+alias cf="code . \$(fzf --multi)"
 alias datestr="date -u +\"%Y-%m-%dT%H-%M-%S%Z\""
-alias f="fzf"
+alias f="fzf --multi"
 alias powershell="powershell.exe"
 alias winpath="wslpath -w"
 
